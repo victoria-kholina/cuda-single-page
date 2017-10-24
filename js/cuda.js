6edcd6ba6a0b0aad1cfd04c71f10479f9d320a2b
victoria-kholina <kholina.victoria@gmail.com>
2017-10-24 21:24:58 +0300

    Form Validation and Animated 'hover styles'  were added.

--- js/cuda.js
+++ js/cuda.js
@@ -15,6 +15,38 @@ function getMenu(){
         $('.menu ul').removeClass("dropdown-menu");     
     };
     };
-        
+    
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 65,
+    startDelay: 500,
+    fadeOut: true,
+    fadeOutDelay: 1000,
+	contentType: "html"
+    //onComplete: function(){ $('.typed-cursor').css('display','none'); }
+  });
+
+$('form').each(function() { 
+    $(this).validate({
+    rules: {
+        name:"required",
+        mail: {
+        required: true,
+        email: true
+      }
+    },
+    messages: {
+      name: "Please enter your firstname",
+      mail: "Please enter a valid email address"
+    },
+    submitHandler: function(form) {
+      form.submit();
+        alert("ok");
+    }
+});
+});
+
+    
 });//end jQuery
+                    
                     
